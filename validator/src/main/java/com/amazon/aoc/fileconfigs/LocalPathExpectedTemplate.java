80a1c3a85108a39a20bcd8ca16b1df89bc51e455
harryryu <harryryu@amazon.com>
2024-02-23 11:26:44 -0800

    Add License Comment

--- validator/src/main/java/com/amazon/aoc/fileconfigs/LocalPathExpectedTemplate.java
+++ validator/src/main/java/com/amazon/aoc/fileconfigs/LocalPathExpectedTemplate.java
@@ -14,6 +14,20 @@
  */
 
 package com.amazon.aoc.fileconfigs;
+/*
+ * Copyright Amazon.com, Inc. or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
 import java.io.IOException;
 import java.net.URL;
