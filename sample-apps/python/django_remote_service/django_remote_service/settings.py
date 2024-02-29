03a0759164df1519dc61fd7f7818150173ed5f0c
Zhonghao Zhao <zzhlogin@amazon.com>
2024-02-29 21:01:00 +0000

    Add STATIC_ROOT for remote service.

--- sample-apps/python/django_remote_service/django_remote_service/settings.py
+++ sample-apps/python/django_remote_service/django_remote_service/settings.py
@@ -1,5 +1,6 @@
 ## Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 ## SPDX-License-Identifier: Apache-2.0
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,6 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
