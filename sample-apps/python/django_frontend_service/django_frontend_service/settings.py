d5bea15202ed5810bd6c5b49e6c4303ab17eb702
Zhonghao Zhao <zzhlogin@amazon.com>
2024-02-29 22:38:30 +0000

    Address comments.

--- sample-apps/python/django_frontend_service/django_frontend_service/settings.py
+++ sample-apps/python/django_frontend_service/django_frontend_service/settings.py
@@ -2,7 +2,9 @@
 ## SPDX-License-Identifier: Apache-2.0
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
