668023d70ac821774c0867a21bff3ac17b85c359
Zhonghao Zhao <zzhlogin@amazon.com>
2024-02-29 22:38:30 +0000

    Address comments.

--- sample-apps/python/django_frontend_service/frontend_service_app/views.py
+++ sample-apps/python/django_frontend_service/frontend_service_app/views.py
@@ -1,29 +1,31 @@
+## Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+## SPDX-License-Identifier: Apache-2.0
 from django.http import HttpResponse, JsonResponse
 import boto3
 import logging
 import requests
 import schedule
 import time
-from threading import Thread
+import threading
 from opentelemetry import trace
-## Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-## SPDX-License-Identifier: Apache-2.0
 from opentelemetry.trace.span import format_trace_id
 
 logger = logging.getLogger(__name__)
 
 should_send_local_root_client_call = False
+lock = threading.Lock()
 def run_local_root_client_call_recurring_service():
     def runnable_task():
         global should_send_local_root_client_call
-        if should_send_local_root_client_call:
-            should_send_local_root_client_call = False
-            try:
-                response = requests.get("http://local-root-client-call")
-                # Handle the response if needed
-            except Exception as e:
-                # Handle exceptions
-                pass
+        with lock:
+            if should_send_local_root_client_call:
+                should_send_local_root_client_call = False
+                try:
+                    response = requests.get("http://local-root-client-call")
+                    # Handle the response if needed
+                except Exception as e:
+                    # Handle exceptions
+                    pass
 
     # Schedule the task to run every 1 second
     schedule.every(1).seconds.do(runnable_task)
@@ -34,7 +36,7 @@ def run_local_root_client_call_recurring_service():
             schedule.run_pending()
             time.sleep(0.1)  # Sleep to prevent high CPU usage
 
-    thread = Thread(target=run_scheduler)
+    thread = threading.Thread(target=run_scheduler)
     thread.daemon = True  # Daemonize the thread so it exits when the main thread exits
     thread.start()
 
@@ -68,8 +70,7 @@ def http_call(request):
 
 def downstream_service(request):
     ip = request.GET.get('ip', '')
-    print("ip!!!!!!!!!!!!!")
-    print(ip)
+    ip = ip.replace("/", "")
     url = f"http://{ip}:8001/health-check"
     try:
         response = requests.get(url)
@@ -86,13 +87,14 @@ def async_service(request):
     # Log the request
     logger.info("Client-call received")
     # Set the condition to trigger the recurring service
-    should_send_local_root_client_call = True
+    with lock:
+        should_send_local_root_client_call = True
     # Return a dummy traceId in the response
     return JsonResponse({'traceId': '1-00000000-000000000000000000000000'})
 
 def get_xray_trace_id():
     span = trace.get_current_span()
     trace_id = format_trace_id(span.get_span_context().trace_id)
-    xray_trace_id = "1-" + trace_id[:8] + "-" + trace_id[8:]
+    xray_trace_id = f"1-{trace_id[:8]}-{trace_id[8:]}"
 
-    return "traceId : " + xray_trace_id
+    return JsonResponse({"traceId": xray_trace_id})
