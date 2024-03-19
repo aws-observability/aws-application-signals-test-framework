707f388d962b7451463734fb353146f33c5b027f
harryryu <harryryu@amazon.com>
2024-03-19 11:11:10 -0700

    Fix Variable Sharing Error in execute_and_retry.sh Script

--- .github/workflows/util/execute_and_retry.sh
+++ .github/workflows/util/execute_and_retry.sh
@@ -5,23 +5,23 @@
 # $2: Command to execute
 # $3: (Optional) Command for cleaning up resources if $2 fails
 execute_and_retry () {
-  retry_counter=0
-  max_retry=$1
+  execute_retry_counter=0
+  max_execute_retry=$1
   command=$2
   cleanup=$3
-  while [ $retry_counter -lt $max_retry ]; do
+  while [ $execute_retry_counter -lt $max_execute_retry ]; do
    attempt_failed=0
    eval "$command" || attempt_failed=$?
 
    if [ $attempt_failed -ne 0 ]; then
      eval "$cleanup"
-     retry_counter=$(($retry_counter+1))
+     execute_retry_counter=$(($execute_retry_counter+1))
      sleep 5
    else
      break
    fi
 
-   if [ $retry_counter -eq $max_retry ]; then
+   if [ $execute_retry_counter -eq $max_execute_retry ]; then
      echo "Max retry reached, command failed to execute properly. Exiting code"
      exit 1
    fi
