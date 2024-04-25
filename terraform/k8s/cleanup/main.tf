315c4134917b083341a6f112c161b852de800000
Lisa Guo <lguo25@gmail.com>
2024-04-24 20:09:00 -0400

    Fix native k8s cleanup script to delete helm-charts repo (#49)

--- terraform/k8s/cleanup/main.tf
+++ terraform/k8s/cleanup/main.tf
@@ -17,8 +17,8 @@ resource "null_resource" "cleanup" {
       # Uninstall the operator and remove the repo from the EC2 instance
       echo "LOG: Uninstalling CloudWatch Agent Operator"
       helm uninstall --debug --namespace amazon-cloudwatch amazon-cloudwatch-operator --ignore-not-found
-      echo "LOG: Deleting CloudWatch Agent Operator repo from environment"
-      [ ! -e amazon-cloudwatch-agent-operator ] || sudo rm -r amazon-cloudwatch-agent-operator
+      echo "LOG: Deleting helm-charts repo from environment"
+      [ ! -e helm-charts ] || sudo rm -r helm-charts
 
       # Delete sample app resources
       echo "LOG: Deleting sample app namespace"
