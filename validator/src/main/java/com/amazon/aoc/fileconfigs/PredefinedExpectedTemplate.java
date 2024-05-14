5c36ead637ed0663b8f22311ec3ddafa9b377e98
harryryu <harryryu@amazon.com>
2024-05-14 11:02:30 -0700

    Resolve Merge Conflict

--- validator/src/main/java/com/amazon/aoc/fileconfigs/PredefinedExpectedTemplate.java
+++ validator/src/main/java/com/amazon/aoc/fileconfigs/PredefinedExpectedTemplate.java
@@ -96,22 +96,39 @@ public enum PredefinedExpectedTemplate implements FileConfig {
   PYTHON_EKS_CLIENT_CALL_METRIC("/expected-data-template/python/eks/client-call-metric.mustache"),
   PYTHON_EKS_CLIENT_CALL_TRACE("/expected-data-template/python/eks/client-call-trace.mustache"),
 
-  /** Python EC2 Test Case Validations */
-  PYTHON_EC2_OUTGOING_HTTP_CALL_LOG("/expected-data-template/python/ec2/outgoing-http-call-log.mustache"),
-  PYTHON_EC2_OUTGOING_HTTP_CALL_METRIC("/expected-data-template/python/ec2/outgoing-http-call-metric.mustache"),
-  PYTHON_EC2_OUTGOING_HTTP_CALL_TRACE("/expected-data-template/python/ec2/outgoing-http-call-trace.mustache"),
+  /** Python EC2 Default Test Case Validations */
+  PYTHON_EC2_DEFAULT_OUTGOING_HTTP_CALL_LOG("/expected-data-template/python/ec2/default/outgoing-http-call-log.mustache"),
+  PYTHON_EC2_DEFAULT_OUTGOING_HTTP_CALL_METRIC("/expected-data-template/python/ec2/default/outgoing-http-call-metric.mustache"),
+  PYTHON_EC2_DEFAULT_OUTGOING_HTTP_CALL_TRACE("/expected-data-template/python/ec2/default/outgoing-http-call-trace.mustache"),
 
-  PYTHON_EC2_AWS_SDK_CALL_LOG("/expected-data-template/python/ec2/aws-sdk-call-log.mustache"),
-  PYTHON_EC2_AWS_SDK_CALL_METRIC("/expected-data-template/python/ec2/aws-sdk-call-metric.mustache"),
-  PYTHON_EC2_AWS_SDK_CALL_TRACE("/expected-data-template/python/ec2/aws-sdk-call-trace.mustache"),
+  PYTHON_EC2_DEFAULT_AWS_SDK_CALL_LOG("/expected-data-template/python/ec2/default/aws-sdk-call-log.mustache"),
+  PYTHON_EC2_DEFAULT_AWS_SDK_CALL_METRIC("/expected-data-template/python/ec2/default/aws-sdk-call-metric.mustache"),
+  PYTHON_EC2_DEFAULT_AWS_SDK_CALL_TRACE("/expected-data-template/python/ec2/default/aws-sdk-call-trace.mustache"),
 
-  PYTHON_EC2_REMOTE_SERVICE_LOG("/expected-data-template/python/ec2/remote-service-log.mustache"),
-  PYTHON_EC2_REMOTE_SERVICE_METRIC("/expected-data-template/python/ec2/remote-service-metric.mustache"),
-  PYTHON_EC2_REMOTE_SERVICE_TRACE("/expected-data-template/python/ec2/remote-service-trace.mustache"),
+  PYTHON_EC2_DEFAULT_REMOTE_SERVICE_LOG("/expected-data-template/python/ec2/default/remote-service-log.mustache"),
+  PYTHON_EC2_DEFAULT_REMOTE_SERVICE_METRIC("/expected-data-template/python/ec2/default/remote-service-metric.mustache"),
+  PYTHON_EC2_DEFAULT_REMOTE_SERVICE_TRACE("/expected-data-template/python/ec2/default/remote-service-trace.mustache"),
 
-  PYTHON_EC2_CLIENT_CALL_LOG("/expected-data-template/python/ec2/client-call-log.mustache"),
-  PYTHON_EC2_CLIENT_CALL_METRIC("/expected-data-template/python/ec2/client-call-metric.mustache"),
-  PYTHON_EC2_CLIENT_CALL_TRACE("/expected-data-template/python/ec2/client-call-trace.mustache"),
+  PYTHON_EC2_DEFAULT_CLIENT_CALL_LOG("/expected-data-template/python/ec2/default/client-call-log.mustache"),
+  PYTHON_EC2_DEFAULT_CLIENT_CALL_METRIC("/expected-data-template/python/ec2/default/client-call-metric.mustache"),
+  PYTHON_EC2_DEFAULT_CLIENT_CALL_TRACE("/expected-data-template/python/ec2/default/client-call-trace.mustache"), 
+    
+  /** Python EC2 Default Test Case Validations */
+  PYTHON_EC2_ASG_OUTGOING_HTTP_CALL_LOG("/expected-data-template/python/ec2/asg/outgoing-http-call-log.mustache"),
+  PYTHON_EC2_ASG_OUTGOING_HTTP_CALL_METRIC("/expected-data-template/python/ec2/asg/outgoing-http-call-metric.mustache"),
+  PYTHON_EC2_ASG_OUTGOING_HTTP_CALL_TRACE("/expected-data-template/python/ec2/asg/outgoing-http-call-trace.mustache"),
+
+  PYTHON_EC2_ASG_AWS_SDK_CALL_LOG("/expected-data-template/python/ec2/asg/aws-sdk-call-log.mustache"),
+  PYTHON_EC2_ASG_AWS_SDK_CALL_METRIC("/expected-data-template/python/ec2/asg/aws-sdk-call-metric.mustache"),
+  PYTHON_EC2_ASG_AWS_SDK_CALL_TRACE("/expected-data-template/python/ec2/asg/aws-sdk-call-trace.mustache"),
+
+  PYTHON_EC2_ASG_REMOTE_SERVICE_LOG("/expected-data-template/python/ec2/asg/remote-service-log.mustache"),
+  PYTHON_EC2_ASG_REMOTE_SERVICE_METRIC("/expected-data-template/python/ec2/asg/remote-service-metric.mustache"),
+  PYTHON_EC2_ASG_REMOTE_SERVICE_TRACE("/expected-data-template/python/ec2/asg/remote-service-trace.mustache"),
+
+  PYTHON_EC2_ASG_CLIENT_CALL_LOG("/expected-data-template/python/ec2/asg/client-call-log.mustache"),
+  PYTHON_EC2_ASG_CLIENT_CALL_METRIC("/expected-data-template/python/ec2/asg/client-call-metric.mustache"),
+  PYTHON_EC2_ASG_CLIENT_CALL_TRACE("/expected-data-template/python/ec2/asg/client-call-trace.mustache"),
   ;
 
   private String path;
