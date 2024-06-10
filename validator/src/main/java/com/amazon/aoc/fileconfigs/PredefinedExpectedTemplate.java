10f03d44fabf60ac46e6127144c6d1a53d53e96c
harryryu <harryryu@amazon.com>
2024-06-10 09:55:13 -0700

    Add back K8s file path variables

--- validator/src/main/java/com/amazon/aoc/fileconfigs/PredefinedExpectedTemplate.java
+++ validator/src/main/java/com/amazon/aoc/fileconfigs/PredefinedExpectedTemplate.java
@@ -127,7 +127,7 @@ public enum PredefinedExpectedTemplate implements FileConfig {
   PYTHON_EC2_DEFAULT_CLIENT_CALL_METRIC("/expected-data-template/python/ec2/default/client-call-metric.mustache"),
   PYTHON_EC2_DEFAULT_CLIENT_CALL_TRACE("/expected-data-template/python/ec2/default/client-call-trace.mustache"), 
     
-  /** Python EC2 Default Test Case Validations */
+  /** Python EC2 Asg Test Case Validations */
   PYTHON_EC2_ASG_OUTGOING_HTTP_CALL_LOG("/expected-data-template/python/ec2/asg/outgoing-http-call-log.mustache"),
   PYTHON_EC2_ASG_OUTGOING_HTTP_CALL_METRIC("/expected-data-template/python/ec2/asg/outgoing-http-call-metric.mustache"),
   PYTHON_EC2_ASG_OUTGOING_HTTP_CALL_TRACE("/expected-data-template/python/ec2/asg/outgoing-http-call-trace.mustache"),
@@ -143,6 +143,23 @@ public enum PredefinedExpectedTemplate implements FileConfig {
   PYTHON_EC2_ASG_CLIENT_CALL_LOG("/expected-data-template/python/ec2/asg/client-call-log.mustache"),
   PYTHON_EC2_ASG_CLIENT_CALL_METRIC("/expected-data-template/python/ec2/asg/client-call-metric.mustache"),
   PYTHON_EC2_ASG_CLIENT_CALL_TRACE("/expected-data-template/python/ec2/asg/client-call-trace.mustache"),
+
+  /** Python K8S Test Case Validations */
+  PYTHON_K8S_OUTGOING_HTTP_CALL_LOG("/expected-data-template/python/k8s/outgoing-http-call-log.mustache"),
+  PYTHON_K8S_OUTGOING_HTTP_CALL_METRIC("/expected-data-template/python/k8s/outgoing-http-call-metric.mustache"),
+  PYTHON_K8S_OUTGOING_HTTP_CALL_TRACE("/expected-data-template/python/k8s/outgoing-http-call-trace.mustache"),
+
+  PYTHON_K8S_AWS_SDK_CALL_LOG("/expected-data-template/python/k8s/aws-sdk-call-log.mustache"),
+  PYTHON_K8S_AWS_SDK_CALL_METRIC("/expected-data-template/python/k8s/aws-sdk-call-metric.mustache"),
+  PYTHON_K8S_AWS_SDK_CALL_TRACE("/expected-data-template/python/k8s/aws-sdk-call-trace.mustache"),
+
+  PYTHON_K8S_REMOTE_SERVICE_LOG("/expected-data-template/python/k8s/remote-service-log.mustache"),
+  PYTHON_K8S_REMOTE_SERVICE_METRIC("/expected-data-template/python/k8s/remote-service-metric.mustache"),
+  PYTHON_K8S_REMOTE_SERVICE_TRACE("/expected-data-template/python/k8s/remote-service-trace.mustache"),
+
+  PYTHON_K8S_CLIENT_CALL_LOG("/expected-data-template/python/k8s/client-call-log.mustache"),
+  PYTHON_K8S_CLIENT_CALL_METRIC("/expected-data-template/python/k8s/client-call-metric.mustache"),
+  PYTHON_K8S_CLIENT_CALL_TRACE("/expected-data-template/python/k8s/client-call-trace.mustache"),
   ;
 
   private String path;
