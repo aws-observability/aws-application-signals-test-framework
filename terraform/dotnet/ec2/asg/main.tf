4d20eb53d2035d1723ff1f8de6e6bbf3170d562d
Xinran Zhang <xinranzh@amazon.com>
2024-07-01 22:10:55 -0700

    asg fixed

--- terraform/dotnet/ec2/asg/main.tf
+++ terraform/dotnet/ec2/asg/main.tf
@@ -145,7 +145,12 @@ resource "aws_launch_configuration" "launch_configuration" {
     export OTEL_RESOURCE_ATTRIBUTES=service.name=dotnet-sample-application-${var.test_id}
     export OTEL_AWS_APPLICATION_SIGNALS_ENABLED=true
     export OTEL_TRACES_SAMPLER=always_on
-    sudo nohup dotnet run &
+
+    echo "$(printenv)" >> /var/log/custom-init.log 2>&1
+    echo "$(sudo -E printenv)" >> /var/log/custom-init.log 2>&1
+
+    sudo -E nohup dotnet run &
+
 
     # The application needs time to come up and reach a steady state, this should not take longer than 30 seconds
     sleep 30
