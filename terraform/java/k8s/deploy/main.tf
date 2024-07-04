d926a04c89ecdf714118e6a97f26ffaaf20448a8
Mahad Janjua <134644284+majanjua-amzn@users.noreply.github.com>
2024-07-04 14:40:26 -0700

    [Fix] Add sleep after patch in java

--- terraform/java/k8s/deploy/main.tf
+++ terraform/java/k8s/deploy/main.tf
@@ -84,6 +84,7 @@ resource "null_resource" "deploy" {
       kubectl patch service sample-app-deployment-${var.test_id} -n sample-app-namespace --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30100}]'
 
       # Wait for sample app to be reach ready state
+      sleep 10
       kubectl wait --for=condition=Ready --request-timeout '5m' pod --all -n sample-app-namespace
 
       # Emit remote service pod IP
