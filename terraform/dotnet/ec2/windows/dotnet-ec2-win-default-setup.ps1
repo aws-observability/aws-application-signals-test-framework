dc89337cf9cd1fbb8a96dd327ebe8e40d6a223f4
Xinran Zhang <xinranzh@amazon.com>
2024-08-15 13:43:48 -0700

    Fix a typo

--- terraform/dotnet/ec2/windows/dotnet-ec2-win-default-setup.ps1
+++ terraform/dotnet/ec2/windows/dotnet-ec2-win-default-setup.ps1
@@ -3,19 +3,17 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 wget -O dotnet-install.ps1 https://dot.net/v1/dotnet-install.ps1
 .\dotnet-install.ps1 -Version 8.0.302
 
-
-$agent_config = '${replace(replace(file("./amazon-cloudwatch-agent.json"), "/\\s+/", ""), "$REGION", "us-east-1")}'
-Set-Content -Path "./amazon-cloudwatch-agent.json" -Value $agent_config
-
-
-
 wget -O .\amazon-cloudwatch-agent.msi https://amazoncloudwatch-agent.s3.amazonaws.com/windows/amd64/latest/amazon-cloudwatch-agent.msi
+
+Write-Host "Installing Cloudwatch Agent"
 msiexec /i amazon-cloudwatch-agent.msi
+Start-Sleep -Seconds 10
+Write-Host "Install Finished"
 
 & "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1" -a fetch-config -m ec2 -s -c file:./amazon-cloudwatch-agent.json
 
 
-Invoke-Expression "wget -O ./aws-distro-opentelemetry-dotnet-instrumentation-windows.zip https://github.com/aws-observability/aws-otel-dotnet-instrumentation/releases/download/v1.1.0/aws-distro-opentelemetry-dotnet-instrumentation-windows.zip}; Expand-Archive -Path ./aws-distro-opentelemetry-dotnet-instrumentation-windows.zip -DestinationPath .\dotnet-distro -Force"
+Invoke-Expression "wget -O ./aws-distro-opentelemetry-dotnet-instrumentation-windows.zip https://github.com/aws-observability/aws-otel-dotnet-instrumentation/releases/download/v1.1.0/aws-distro-opentelemetry-dotnet-instrumentation-windows.zip; Expand-Archive -Path ./aws-distro-opentelemetry-dotnet-instrumentation-windows.zip -DestinationPath .\dotnet-distro -Force"
 
 
 wget -O ./dotnet-sample-app.zip https://github.com/aws-observability/aws-application-signals-test-framework/raw/dotnetE2ETests/sample-apps/dotnet/dotnet-sample-app.zip
