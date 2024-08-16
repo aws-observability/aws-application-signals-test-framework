8dfd56554d3feb1261bb7b92f350ce45cce3c2ae
Xinran Zhang <xinranzh@amazon.com>
2024-08-16 10:56:28 -0700

    param

--- terraform/dotnet/ec2/windows/dotnet-ec2-win-default-setup.ps1
+++ terraform/dotnet/ec2/windows/dotnet-ec2-win-default-setup.ps1
@@ -1,9 +1,17 @@
+param (
+    [string]$GetCloudwatchAgentCommand,
+    [string]$GetAdotDistroCommand,
+    [string]$GetSampleAppCommand
+)
+
+
 msiexec.exe /i https://awscli.amazonaws.com/AWSCLIV2.msi
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 wget -O dotnet-install.ps1 https://dot.net/v1/dotnet-install.ps1
 .\dotnet-install.ps1 -Version 8.0.302
 
-wget -O .\amazon-cloudwatch-agent.msi https://amazoncloudwatch-agent.s3.amazonaws.com/windows/amd64/latest/amazon-cloudwatch-agent.msi
+Invoke-Expression $GetCloudwatchAgentCommand
+#wget -O .\amazon-cloudwatch-agent.msi https://amazoncloudwatch-agent.s3.amazonaws.com/windows/amd64/latest/amazon-cloudwatch-agent.msi
 
 Write-Host "Installing Cloudwatch Agent"
 msiexec /i amazon-cloudwatch-agent.msi
@@ -12,13 +20,15 @@ Write-Host "Install Finished"
 
 & "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1" -a fetch-config -m ec2 -s -c file:./amazon-cloudwatch-agent.json
 
+Invoke-Expression $GetAdotDistroCommand
+#Invoke-Expression "wget -O ./aws-distro-opentelemetry-dotnet-instrumentation-windows.zip https://github.com/aws-observability/aws-otel-dotnet-instrumentation/releases/download/v1.1.0/aws-distro-opentelemetry-dotnet-instrumentation-windows.zip; Expand-Archive -Path ./aws-distro-opentelemetry-dotnet-instrumentation-windows.zip -DestinationPath .\dotnet-distro -Force"
 
-Invoke-Expression "wget -O ./aws-distro-opentelemetry-dotnet-instrumentation-windows.zip https://github.com/aws-observability/aws-otel-dotnet-instrumentation/releases/download/v1.1.0/aws-distro-opentelemetry-dotnet-instrumentation-windows.zip; Expand-Archive -Path ./aws-distro-opentelemetry-dotnet-instrumentation-windows.zip -DestinationPath .\dotnet-distro -Force"
-
-
-wget -O ./dotnet-sample-app.zip https://github.com/aws-observability/aws-application-signals-test-framework/raw/dotnetE2ETests/sample-apps/dotnet/dotnet-sample-app.zip
+Invoke-Expression $GetSampleAppCommand
+#wget -O ./dotnet-sample-app.zip https://github.com/aws-observability/aws-application-signals-test-framework/raw/dotnetE2ETests/sample-apps/dotnet/dotnet-sample-app.zip
 
 Expand-Archive -Path .\dotnet-sample-app.zip -DestinationPath .\ -Force
+
+New-NetFirewallRule -DisplayName "Allow TCP 8080" -Direction Inbound -Protocol TCP -LocalPort 8080 -Action Allow
 
 $current_dir = Get-Location
 Write-Host $current_dir
