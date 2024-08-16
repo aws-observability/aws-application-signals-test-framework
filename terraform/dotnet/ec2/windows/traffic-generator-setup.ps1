f1388d9ed5d87ae4f9dfb6bee5ab8dbfd5900433
Xinran Zhang <xinranzh@amazon.com>
2024-08-16 15:02:24 -0700

    line wrap problem fix

--- terraform/dotnet/ec2/windows/traffic-generator-setup.ps1
+++ terraform/dotnet/ec2/windows/traffic-generator-setup.ps1
@@ -12,8 +12,7 @@ $env:Path += ";$currentdir" + "\nodejs\node-v20.16.0-win-x64"
 
 # Bring in the traffic generator files to EC2 Instance
 #aws s3 cp "s3://aws-appsignals-sample-app-prod-${var.aws_region}/traffic-generator.zip" "./traffic-generator.zip"
-wget -o traffic-generator.zip https://raw.githubusercontent
-.com/aws-observability/aws-application-signals-test-framework/dotnetMergeBranch-windows/terraform/dotnet/ec2/windows/traffic-generator.zip
+wget -o traffic-generator.zip https://raw.githubusercontent.com/aws-observability/aws-application-signals-test-framework/dotnetMergeBranch-windows/terraform/dotnet/ec2/windows/traffic-generator.zip
 Expand-Archive -Path "./traffic-generator.zip" -DestinationPath "./"
 
 # Install the traffic generator dependencies
