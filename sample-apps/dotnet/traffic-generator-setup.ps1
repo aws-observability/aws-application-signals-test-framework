dcb90ef571979d25669294930f67b05a03fd3841
Xinran Zhang <xinranzh@amazon.com>
2024-09-05 14:17:06 -0700

    do not install aws-cli on script

--- sample-apps/dotnet/traffic-generator-setup.ps1
+++ sample-apps/dotnet/traffic-generator-setup.ps1
@@ -4,19 +4,7 @@ param (
     [string]$TestCanaryType,
     [string]$AWSRegion
 )
-
-msiexec.exe /i https://awscli.amazonaws.com/AWSCLIV2.msi /qn
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-$awsCliInstalled = Get-Command aws -ErrorAction SilentlyContinue
-
-while (-not $awsCliInstalled) {
-    Write-Host "Waiting for AWS CLI"
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-    $awsCliInstalled = Get-Command aws -ErrorAction SilentlyContinue
-    Write-Host "Waiting"
-    Start-Sleep -Seconds 5
-}
-Write-Host "AWS CLI Installed"
+$ProgressPreference = 'SilentlyContinue'
 
 wget -O nodejs.zip https://nodejs.org/dist/v20.16.0/node-v20.16.0-win-x64.zip
 Expand-Archive -Path .\nodejs.zip -DestinationPath .\nodejs -Force
