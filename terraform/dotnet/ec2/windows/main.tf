524f878a7f4ae6c5c5bd62bc2d1095afa83a2a1f
Xinran Zhang <xinranzh@amazon.com>
2024-09-12 16:07:47 -0700

    use windows 2022

--- terraform/dotnet/ec2/windows/main.tf
+++ terraform/dotnet/ec2/windows/main.tf
@@ -25,7 +25,7 @@ data "aws_ami" "ami" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["Windows_Server-2019-English-Full-Base-*"]
+    values = ["Windows_Server-2022-English-Full-Base-*"]
   }
   filter {
     name   = "state"
