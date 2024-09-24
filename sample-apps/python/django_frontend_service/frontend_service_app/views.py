c2182e7fdfe993b10ae39c3f4a28d17b127eadd6
Mingjie Shao <mingjs@amazon.com>
2024-09-24 18:22:15 +0100

    [Python/EKS] Fix credential interpolation issue in Enablement Canary Test (#249)

--- sample-apps/python/django_frontend_service/frontend_service_app/views.py
+++ sample-apps/python/django_frontend_service/frontend_service_app/views.py
@@ -2,6 +2,7 @@
 ## SPDX-License-Identifier: Apache-2.0
 import logging
 import os
+import base64
 import threading
 import time
 
@@ -110,10 +111,14 @@ def get_xray_trace_id():
 
 def mysql(request):
     logger.info("mysql received")
+
+    encoded_password = os.environ["RDS_MYSQL_CLUSTER_PASSWORD"]
+    decoded_password = base64.b64decode(encoded_password).decode('utf-8')
+
     try:
         connection = pymysql.connect(host=os.environ["RDS_MYSQL_CLUSTER_ENDPOINT"],
                                      user=os.environ["RDS_MYSQL_CLUSTER_USERNAME"],
-                                     password=os.environ["RDS_MYSQL_CLUSTER_PASSWORD"],
+                                     password=decoded_password,
                                      database=os.environ["RDS_MYSQL_CLUSTER_DATABASE"])
         with connection:
             with connection.cursor() as cursor:
