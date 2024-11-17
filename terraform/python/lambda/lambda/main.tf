7f4c6084a40f0963bafb7ce029080eabc89dc20d
Lei Wang <66336933+wangzlei@users.noreply.github.com>
2024-11-17 09:44:30 -0800

    Update main.tf

--- terraform/python/lambda/lambda/main.tf
+++ terraform/python/lambda/lambda/main.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_layer_version" "sdk_layer" {
   count               = var.is_canary ? 0 : 1
   layer_name          = var.sdk_layer_name
   filename            = "${var.layer_artifacts_directory}/layer.zip"
-  compatible_runtimes = ["python3.10", "python3.11", "python3.12"]
+  compatible_runtimes = ["python3.10", "python3.11", "python3.12", "python3.13"]
   license_info        = "Apache-2.0"
   source_code_hash    = filebase64sha256("${var.layer_artifacts_directory}/layer.zip")
 #   filename = "${var.kube_directory_path}/config"
