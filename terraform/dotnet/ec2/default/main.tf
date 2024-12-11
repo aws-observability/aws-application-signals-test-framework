9664a3b720d1ba95a18a649368d68f961d595030
Jeel Mehta <jeelmm@amazon.com>
2024-12-11 15:40:31 -0800

    Testing EC2

--- terraform/dotnet/ec2/default/main.tf
+++ terraform/dotnet/ec2/default/main.tf
@@ -307,7 +307,7 @@ resource "null_resource" "traffic_generator_setup" {
         sudo yum install nodejs aws-cli unzip tmux -y
 
         # Bring in the traffic generator files to EC2 Instance
-        aws s3 cp s3://aws-appsignals-sample-app-prod-${var.aws_region}/traffic-generator.zip ./traffic-generator.zip
+        aws s3 cp s3://aws-appsignals-sample-app-prod-jeel/traffic-generator.zip ./traffic-generator.zip
         unzip ./traffic-generator.zip -d ./
 
         # Install the traffic generator dependencies
