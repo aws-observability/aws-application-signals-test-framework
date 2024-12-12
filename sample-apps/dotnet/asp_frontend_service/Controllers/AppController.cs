58fd919af70f8dfa74d17e10ad93ac0ccdb959e6
Jeel Mehta <jeelmm@amazon.com>
2024-12-12 11:52:53 -0800

    Testing eks

--- sample-apps/dotnet/asp_frontend_service/Controllers/AppController.cs
+++ sample-apps/dotnet/asp_frontend_service/Controllers/AppController.cs
@@ -61,7 +61,6 @@ public class AppController : ControllerBase
     public string OutgoingHttp()
     {
         _ = this.httpClient.GetAsync("https://aws.amazon.com").Result;
-
         return this.GetTraceId();
     }
 
