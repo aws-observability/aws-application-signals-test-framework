af5042805dc7408d15e8dd544af09f10541c3590
Jeel-mehta <72543735+Jeel-mehta@users.noreply.github.com>
2025-01-27 18:48:32 -0800

    Add ubuntu test, terraform and validation files (#364)
    *Issue description:* Existing testing framework did not have ubuntu test
    for java
    
    *Description of changes:* Added java ec2 ubuntu test along with
    terraform files and validation files
    
    Test link:
    https://github.com/Jeel-mehta/aws-application-signals-test-framework/actions/runs/13001021774
    
    *Rollback procedure:*
    
    <Can we safely revert this commit if needed? If not, detail what must be
    done to safely revert and why it is needed.>
    
    *Ensure you've run the following tests on your changes and include the
    link below:*
    
    To do so, create a `test.yml` file with `name: Test` and workflow
    description to test your changes, then remove the file for your PR. Link
    your test run in your PR description. This process is a short term
    solution while we work on creating a staging environment for testing.
    
    NOTE: TESTS RUNNING ON A SINGLE EKS CLUSTER CANNOT BE RUN IN PARALLEL.
    See the
    [needs](https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idneeds)
    keyword to run tests in succession.
    - Run Java EKS on `e2e-playground` in us-east-1 and eu-central-2
    - Run Python EKS on `e2e-playground` in us-east-1 and eu-central-2
    - Run metric limiter on EKS cluster `e2e-playground` in us-east-1 and
    eu-central-2
    - Run EC2 tests in all regions
    - Run K8s on a separate K8s cluster (check IAD test account for master
    node endpoints; these will change as we create and destroy clusters for
    OS patching)
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    
    ---------
    
    Co-authored-by: Jeel Mehta <jeelmm@amazon.com>

---
+++ terraform/java/ec2/ubuntu/main.tf
@@ -0,0 +1,323 @@
+# ------------------------------------------------------------------------
+# Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+# -------------------------------------------------------------------------
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# Define the provider for AWS
+provider "aws" {}
+
+resource "aws_default_vpc" "default" {}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "aws_key_pair" "aws_ssh_key" {
+  key_name = "instance_key-${var.test_id}"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+locals {
+  ssh_key_name        = aws_key_pair.aws_ssh_key.key_name
+  private_key_content = tls_private_key.ssh_key.private_key_pem
+}
+
+data "aws_ami" "ami" {
+  owners = ["amazon"]
+  most_recent      = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  
+  filter {
+    name   = "image-type"
+    values = ["machine"]
+  }
+
+  filter {
+    name   = "root-device-name"
+    values = ["/dev/sda1"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "main_service_instance" {
+  ami                                   = data.aws_ami.ami.id 
+  instance_type                         = "t3.micro" 
+  key_name                              = local.ssh_key_name
+  iam_instance_profile                  = "APP_SIGNALS_EC2_TEST_ROLE"
+  vpc_security_group_ids                = [aws_default_vpc.default.default_security_group_id]
+  associate_public_ip_address           = true
+  instance_initiated_shutdown_behavior  = "terminate"
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    volume_size = 8
+  }
+
+  tags = {
+    Name = "main-service-${var.test_id}"
+  }
+}
+
+resource "null_resource" "main_service_setup" {
+  connection {
+    type = "ssh"
+    user = var.user
+    private_key = local.private_key_content
+    host = aws_instance.main_service_instance.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      <<-EOF
+      #!/bin/bash
+      # Make the Terraform fail if any step throws an error
+      set -o errexit
+      # Install wget and Java based on OS
+      # Ubuntu commands
+      sudo apt-get update
+      sudo apt-get install -y software-properties-common
+      sudo add-apt-repository -y ppa:openjdk-r/ppa
+      sudo apt-get update
+      sudo apt-get install wget -y
+      # Try to fix broken packages if any
+      sudo apt-get install -y -f
+      # Clean apt cache
+      sudo apt-get clean
+      sudo apt-get autoremove -y
+      sudo apt-get install -y openjdk-11-jdk 
+      sudo apt-get install -y awscli
+      
+      # Copy in CW Agent configuration
+      agent_config='${replace(replace(file("./amazon-cloudwatch-agent.json"), "/\\s+/", ""), "$REGION", var.aws_region)}'
+      echo $agent_config > amazon-cloudwatch-agent.json
+
+      # Get and run CW agent rpm/ubuntu
+      ${var.get_cw_agent_deb_command}
+      sudo dpkg -i -E ./cw-agent.deb
+      sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:./amazon-cloudwatch-agent.json
+
+      # Get ADOT
+      ${var.get_adot_jar_command}
+
+      # Get and run the sample application with configuration
+      aws s3 cp ${var.sample_app_jar} ./main-service.jar
+
+      JAVA_TOOL_OPTIONS=' -javaagent:./adot.jar' \
+      OTEL_METRICS_EXPORTER=none \
+      OTEL_LOGS_EXPORT=none \
+      OTEL_AWS_APPLICATION_SIGNALS_ENABLED=true \
+      OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT=http://localhost:4316/v1/metrics \
+      OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf \
+      OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://localhost:4316/v1/traces \
+      OTEL_RESOURCE_ATTRIBUTES=service.name=sample-application-${var.test_id} \
+      OTEL_INSTRUMENTATION_COMMON_EXPERIMENTAL_CONTROLLER_TELEMETRY_ENABLED=true \
+      nohup java -XX:+UseG1GC -jar main-service.jar &> nohup.out &
+
+      # The application needs time to come up and reach a steady state, this should not take longer than 30 seconds
+      sleep 30
+
+      # Check if the application is up. If it is not up, then exit 1.
+      attempt_counter=0
+      max_attempts=30
+      until $(curl --output /dev/null --silent --head --fail --max-time 5 $(echo "http://localhost:8080" | tr -d '"')); do
+        if [ $attempt_counter -eq $max_attempts ];then
+          echo "Failed to connect to endpoint."
+          exit 1
+        fi
+        echo "Attempting to connect to the main endpoint. Tried $attempt_counter out of $max_attempts"
+        attempt_counter=$(($attempt_counter+1))
+        sleep 10
+      done
+
+      echo "Successfully connected to main endpoint"
+
+      EOF
+    ]
+  }
+
+  depends_on = [aws_instance.main_service_instance]
+}
+
+resource "aws_instance" "remote_service_instance" {
+  ami                                   = data.aws_ami.ami.id 
+  instance_type                         = "t3.micro"
+  key_name                              = local.ssh_key_name
+  iam_instance_profile                  = "APP_SIGNALS_EC2_TEST_ROLE"
+  vpc_security_group_ids                = [aws_default_vpc.default.default_security_group_id]
+  associate_public_ip_address           = true
+  instance_initiated_shutdown_behavior  = "terminate"
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    volume_size = 8
+  }
+
+  tags = {
+    Name = "remote-service-${var.test_id}"
+  }
+}
+
+resource "null_resource" "remote_service_setup" {
+  connection {
+    type = "ssh"
+    user = var.user
+    private_key = local.private_key_content
+    host = aws_instance.remote_service_instance.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      <<-EOF
+      #!/bin/bash
+      # Make the Terraform fail if any step throws an error
+      set -o errexit
+      # Install wget and Java 
+      # Ubuntu commands
+      sudo apt-get update
+      sudo apt-get install -y software-properties-common
+      sudo add-apt-repository -y ppa:openjdk-r/ppa
+      sudo apt-get update
+      sudo apt-get install wget -y
+      # Try to fix broken packages if any
+      sudo apt-get install -y -f
+      # Clean apt cache
+      sudo apt-get clean
+      sudo apt-get autoremove -y
+      sudo apt-get install -y openjdk-11-jdk
+      sudo apt-get install -y awscli
+      sudo apt-get update
+      sudo apt-get install ec2-instance-connect -y
+      
+      # Copy in CW Agent configuration
+      agent_config='${replace(replace(file("./amazon-cloudwatch-agent.json"), "/\\s+/", ""), "$REGION", var.aws_region)}'
+      echo $agent_config > amazon-cloudwatch-agent.json
+
+      # Get and run CW agent rpm/ubuntu
+      ${var.get_cw_agent_deb_command}
+      sudo dpkg -i -E ./cw-agent.deb
+      sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:./amazon-cloudwatch-agent.json
+
+      # Get ADOT
+      ${var.get_adot_jar_command}
+
+      # Get and run the sample application with configuration
+      aws s3 cp ${var.sample_remote_app_jar} ./remote-service.jar
+
+      JAVA_TOOL_OPTIONS=' -javaagent:./adot.jar' \
+      OTEL_METRICS_EXPORTER=none \
+      OTEL_LOGS_EXPORT=none \
+      OTEL_AWS_APPLICATION_SIGNALS_ENABLED=true \
+      OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT=http://localhost:4316/v1/metrics \
+      OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf \
+      OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://localhost:4316/v1/traces \
+      OTEL_RESOURCE_ATTRIBUTES=service.name=sample-remote-application-${var.test_id} \
+      OTEL_INSTRUMENTATION_COMMON_EXPERIMENTAL_CONTROLLER_TELEMETRY_ENABLED=true \
+      nohup java -XX:+UseG1GC -jar remote-service.jar &> nohup.out &
+
+      # The application needs time to come up and reach a steady state, this should not take longer than 30 seconds
+      sleep 30
+
+      # Check if the application is up. If it is not up, then exit 1.
+      attempt_counter=0
+      max_attempts=30
+      until $(curl --output /dev/null --silent --head --fail --max-time 5 $(echo "http://localhost:8080/healthcheck" | tr -d '"')); do
+        if [ $attempt_counter -eq $max_attempts ];then
+          echo "Failed to connect to endpoint."
+          exit 1
+        fi
+        echo "Attempting to connect to the remote endpoint. Tried $attempt_counter out of $max_attempts"
+        attempt_counter=$(($attempt_counter+1))
+        sleep 10
+      done
+
+      echo "Successfully connected to remote endpoint"
+
+      EOF
+    ]
+  }
+
+  depends_on = [aws_instance.remote_service_instance]
+}
+
+resource "null_resource" "traffic_generator_setup" {
+  connection {
+    type = "ssh"
+    user = var.user
+    private_key = local.private_key_content
+    host = aws_instance.main_service_instance.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      <<-EOF
+      #!/bin/bash
+        # Ubuntu commands
+        sudo apt-get update
+        # Install curl first if not already installed
+        sudo apt-get install -y curl
+        # Install Node.js and npm using NodeSource
+        curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+        sudo apt-get install -y nodejs
+        # Install other required packages
+        sudo apt-get install -y awscli unzip tmux
+
+        # Bring in the traffic generator files to EC2 Instance
+        aws s3 cp s3://aws-appsignals-sample-app-prod-${var.aws_region}/traffic-generator.zip ./traffic-generator.zip
+        unzip ./traffic-generator.zip -d ./
+
+        # Install the traffic generator dependencies
+        npm install
+
+        tmux new -s traffic-generator -d
+        tmux send-keys -t traffic-generator "export MAIN_ENDPOINT=\"localhost:8080\"" C-m
+        tmux send-keys -t traffic-generator "export REMOTE_ENDPOINT=\"${aws_instance.remote_service_instance.private_ip}\"" C-m
+        tmux send-keys -t traffic-generator "export ID=\"${var.test_id}\"" C-m
+        tmux send-keys -t traffic-generator "npm start" C-m
+
+      EOF
+    ]
+  }
+
+  depends_on = [null_resource.main_service_setup, null_resource.remote_service_setup]
+}
