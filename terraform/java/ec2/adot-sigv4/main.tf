db9b1441c9675922122fa905b8c3c51fecd42e53
Jeel Mehta <jeelmm@amazon.com>
2025-05-14 15:31:25 -0700

    Adding new environment variable

--- terraform/java/ec2/adot-sigv4/main.tf
+++ terraform/java/ec2/adot-sigv4/main.tf
@@ -137,6 +137,7 @@ resource "null_resource" "main_service_setup" {
       export OTEL_TRACES_EXPORTER=otlp
       export OTEL_EXPORTER_OTLP_TRACES_PROTOCOL=http/protobuf
       export OTEL_EXPORTER_OTLP_LOGS_PROTOCOL=http/protobuf
+      export OTEL_LOG_LEVEL=CRITICAL
       export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=https://xray.${var.aws_region}.amazonaws.com/v1/traces
       export OTEL_EXPORTER_OTLP_LOGS_ENDPOINT=https://logs.${var.aws_region}.amazonaws.com/v1/logs
       export OTEL_EXPORTER_OTLP_LOGS_HEADERS=x-aws-log-group=otlp_sigv4_logs,x-aws-log-stream=default
@@ -228,6 +229,7 @@ resource "null_resource" "remote_service_setup" {
       export OTEL_TRACES_EXPORTER=otlp
       export OTEL_EXPORTER_OTLP_TRACES_PROTOCOL=http/protobuf
       export OTEL_EXPORTER_OTLP_LOGS_PROTOCOL=http/protobuf
+      export OTEL_LOG_LEVEL=CRITICAL
       export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=https://xray.${var.aws_region}.amazonaws.com/v1/traces
       export OTEL_EXPORTER_OTLP_LOGS_ENDPOINT=https://logs.${var.aws_region}.amazonaws.com/v1/logs
       export OTEL_EXPORTER_OTLP_LOGS_HEADERS=x-aws-log-group=otlp_sigv4_logs,x-aws-log-stream=default
