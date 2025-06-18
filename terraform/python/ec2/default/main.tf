6847ac3685865f576380598d38413e7df601c6cd
Michael He <53622546+yiyuan-he@users.noreply.github.com>
2025-06-18 15:10:18 -0700

    Add Missing Python 3.13 support in Part of the EC2 workflow (#417)
    ## What does this pull request do?
    Missed adding Python 3.13 support in one part of the EC2 workflow:
    https://github.com/aws-observability/aws-application-signals-test-framework/pull/415
    
    This should fix the EC2 failure in [main
    build](https://github.com/aws-observability/aws-otel-python-instrumentation/actions/runs/15741464564/job/44371235567).
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- terraform/python/ec2/default/main.tf
+++ terraform/python/ec2/default/main.tf
@@ -249,7 +249,7 @@ resource "null_resource" "remote_service_setup" {
       # Dnf does not have the module for python 3.10, 3,10, 3.12, therefore we need to manually install it by downloading the package from the python website.
       # Building and installing the package takes longer then installing it through dnf, so a seperate installation process was made.
       # The canary should run on a version without the manual installation process
-      if [ "${var.language_version}" == "3.8" ] || [ "${var.language_version}" = "3.10" ] || [ "${var.language_version}" = "3.12" ]; then
+      if [ "${var.language_version}" == "3.8" ] || [ "${var.language_version}" = "3.10" ] || [ "${var.language_version}" = "3.12" ] || [ "${var.language_version}" = "3.13" ]; then
           # Install modules required to compile Python and also run the sample app
           sudo dnf groupinstall "Development Tools" -y
           sudo dnf install openssl-devel sqlite-devel libffi-devel -y
