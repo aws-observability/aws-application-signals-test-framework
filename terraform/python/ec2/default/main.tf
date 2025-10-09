bc4a37a1366a3ea95add8145e03cb9c836166d23
Vherremi <vherremi@amazon.com>
2025-10-09 12:33:36 -0700

    fixing typo in main.tf

--- terraform/python/ec2/default/main.tf
+++ terraform/python/ec2/default/main.tf
@@ -145,7 +145,7 @@ resource "null_resource" "main_service_setup" {
       sudo yum install ec2-instance-connect -y
 
       # Copy in CW Agent configuration
-      agent_config='${replace(replace(file(var.custom_metrics_enabled ? "./amazon-cloudwatch-custom-agent.json" : "./amazon-cloudwatch-agent.json"), "/\\s+/", ""), "$REGION", var.aws_region)}'
+      agent_config='${replace(file(var.custom_metrics_enabled ? "./amazon-cloudwatch-custom-agent.json" : "./amazon-cloudwatch-agent.json"), "$REGION", var.aws_region)}'
       echo $agent_config > amazon-cloudwatch-agent.json
 
       # Get and run CW agent rpm
