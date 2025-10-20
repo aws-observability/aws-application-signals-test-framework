e1717f119ea82ce3b7ab7aa0cbd02f4b9f53b513
Vherremi <vherremi@amazon.com>
2025-10-20 10:10:09 -0700

    testing 2nd round pr updates

--- terraform/python/ec2/default/variables.tf
+++ terraform/python/ec2/default/variables.tf
@@ -48,11 +48,3 @@ variable "language_version" {
 variable "cpu_architecture" {
   default = "x86_64"
 }
-
-#Adding Custom Metrics Variables
-
-variable "custom_metrics_enabled" {
-  description = "Enable custom OTEL metrics in the sample application"
-  type        = bool
-  default     = false
-}
