58a17d5249c508e50b9047a7c3b1f8d47943e09d
Vherremi <vherremi@amazon.com>
2025-11-12 09:01:26 -0800

    dotnet-ec2-default test 13

--- terraform/dotnet/ec2/default/main.tf
+++ terraform/dotnet/ec2/default/main.tf
@@ -154,8 +154,13 @@ resource "null_resource" "main_service_setup" {
       export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4316/v1/metrics
       export OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT=http://localhost:4316/v1/metrics
       export OTEL_METRICS_EXPORTER=otlp
+      export OTEL_TRACES_EXPORTER=otlp
+      export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://localhost:4316/v1/traces
+      export OTEL_EXPORTER_OTLP_TRACES_PROTOCOL=http/protobuf
       export OTEL_EXPORTER_OTLP_METRICS_ENDPOINT=http://localhost:4318/v1/metrics
       export OTEL_EXPORTER_OTLP_METRICS_PROTOCOL=http/protobuf
+      export OTEL_EXPORTER_OTLP_METRICS_INSECURE=true 
+      export OTEL_DOTNET_AUTO_METRICS_ADDITIONAL_SOURCES=myMeter
       export SERVICE_NAME='dotnet-sample-application-${var.test_id}'
       export DEPLOYMENT_ENVIRONMENT_NAME='ec2:default'
       export OTEL_RESOURCE_ATTRIBUTES="service.name=$${SERVICE_NAME},deployment.environment.name=$${DEPLOYMENT_ENVIRONMENT_NAME}"
